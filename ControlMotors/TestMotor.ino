26a4fcae69b8fe3de2dfe44196db08d20b505859
Kevin Medrano Ayala <kevin.ejem18@gmail.com>
2022-03-28 10:04:49 +0200

    Update Test Motor v0.1

--- ControlMotors/TestMotor.ino
+++ ControlMotors/TestMotor.ino
@@ -4,7 +4,16 @@
 #define OUT_STEP_M2 7
 #define OUT_DIR_M2 8
 #define OUT_ENABLE_M1 4
+#define OUT_ENABLE_M1 5
 
+unsigned long tiempo_rev = 0;
+unsigned long tiempo_revF = 0;
+// DMODE0 > 0, DMODE1 > 0, DMODE2 > 1 ==> Paso Completo
+// DMODE0 > 1, DMODE1 > 1, DMODE2 > 1 ==> 1/32 
+
+int DMODE0 = 9;
+int DMODE1 = 10;
+int DMODE2 = 11;
 void setup() {
   // put your setup code here, to run once:
     pinMode(OUT_STEP_M1, OUTPUT);
@@ -12,19 +21,32 @@ void setup() {
     pinMode(OUT_STEP_M2, OUTPUT);
     pinMode(OUT_DIR_M2, OUTPUT);
     pinMode(OUT_ENABLE_M1, OUTPUT);
+    pinMode(DMODE0, OUTPUT);
+    pinMode(DMODE1, OUTPUT);
+    pinMode(DMODE2, OUTPUT);
+    Serial.begin(9600);
+    digitalWrite(OUT_ENABLE_M1, 1);
+    // Configuracoin de velocidad por pasos
+    digitalWrite(DMODE0, 0);
+    digitalWrite(DMODE1, 0);
+    digitalWrite(DMODE2, 1);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
     digitalWrite(OUT_DIR_M1, 1);
     digitalWrite(OUT_DIR_M1, 2);
-    for (int i = 0; i < 200; i++)
-    {
-        digitalWrite(OUT_STEP_M1, 1);
-        digitalWrite(OUT_STEP_M2, 1);
-        delay(5);
-        digitalWrite(OUT_STEP_M2, 0);
-        digitalWrite(OUT_STEP_M2, 0);
-        delay(5);
-    }
+    tiempo_rev = millis();
+    Serial.print("/nTiempo Inicial:")
+    Serial.print(tiempo_rev)
+    digitalWrite(OUT_STEP_M1, 1);
+    digitalWrite(OUT_STEP_M2, 1);
+    delayMicroseconds(340);
+    digitalWrite(OUT_STEP_M2, 0);
+    digitalWrite(OUT_STEP_M2, 0);
+    delayMicroseconds(340);
+    tiempo_revF = millis();
+    Serial.print("/nTiempo Final")
+    Serial.print(tiempo_revF)
+    Serial.println("")
 }
